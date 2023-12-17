c62450bf3fec4e25643ce58f51ac6c27bd2f025c
im_pig_ggul_ggul <141539089+pigggulggul@users.noreply.github.com>
2023-12-17 23:08:28 +0900

    add : template

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import CanvasItem from "./CanvasItem";
 import useClientWidthHeight from "./script/useClientWidthHeight";
 import html2canvas from "html2canvas";
+import TypeA from "./component/TypeA";
 
 type templateType = {
   thumb: string;
@@ -60,77 +61,15 @@ function App() {
     };
     onCapture();
   };
-  const upload = (event: any) => {
-    console.log("upload 호출");
-    console.log(event.target.files.length);
-    setPreviewImages("");
-    if (event.target.files.length > 1) {
-      alert("사진은 최대 1개 까지 첨부가능합니다.");
-    } else {
-      //file째로(DTO-X 뷰에 찍히는 정보대로) 저장
-      // files.value.fileInfos = event.target.files;
-      for (const file of event.target.files) {
-        //프리뷰
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          setPreviewImages(e.target.result);
-          console.log(previewImages);
-        };
-        reader.readAsDataURL(file);
-      }
-    }
-    // 사용자가 올린 이미지
-    console.log(event.target.files);
-    // URL.createObjectURL로 사용자가 올린 이미지를 URL로 만들어서 화면에 표시할 수 있게 한다. img 태그의 src값에 바인딩해준다
-    //   this.imageUploaded = URL.createObjectURL(this.image)
-  };
+
   return (
     <div className="flex flex-col justify-center items-center">
-      <section
-        className="main flex justify-center items-center w-3/4"
-        style={{ height: "800px" }}
-      >
-        <main id="capture" className="w-3/4 h-full relative" ref={canvasRef}>
-          <CanvasItem
-            canvasWidth={canvasWidth}
-            canvasHeight={canvasHeight}
-            onChangeType={currentTemplate}
-          ></CanvasItem>
-
-          <div className="absolute w-3/4 mb-4 bottom-0 border left-1/2 -translate-x-1/2">
-            <div className="w-full flex h-64 bg-slate-100">
-              <div className="w-3/4 flex flex-col justify-between border p-2">
-                <p className="frame-text-1">
-                  I confess, I haven't tried too hard to concentrate, not when I
-                  know you are but a daydreamaway.
-                </p>
-                <span className="text-right">icon</span>
-              </div>
-              <div className="w-1/4 flex flex-col justify-center items-center border p-2">
-                <img className="w-5/6 h-5/6" src={previewImages} />
-                <p className="frame-text-2 h-1/6 flex justify-center items-center">
-                  Eliet
-                </p>
-              </div>
-            </div>
-          </div>
-        </main>
-        <div className="detail w-1/4 h-full border">
-          <div className="mypage-img-wrap">
-            <label className="input-file-button" htmlFor="upfile">
-              <span className=""> 사진등록 </span>
-            </label>
-            <input
-              type="file"
-              id="upfile"
-              accept="image/*, .gif"
-              onChange={upload}
-              style={{ display: "none" }}
-            />
-          </div>
-        </div>
-      </section>
-
+      <TypeA
+        canvasRef={canvasRef}
+        canvasWidth={canvasWidth}
+        canvasHeight={canvasHeight}
+        template={currentTemplate}
+      ></TypeA>
       {/* template */}
       <section className="template w-3/4">
         <ul className="flex border border-red-100">{templateList}</ul>
